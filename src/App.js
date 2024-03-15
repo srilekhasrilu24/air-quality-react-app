0a275fdb77b4c8d32a3fecd39dd34427b0020961
srilekhasrilu24 <srilekhaambati71@gmail.com>
2024-03-15 10:16:46 +0530

    air quality app

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+
+import React from 'react';
+import './App.css';
+import MajorProject from './majorproject/majorproject';
+// import GetApi from './majorproject/API/userInterface';
+
+
+
+function App() {
+  return (
+    <>
+    <MajorProject />
+    {/* <GetApi/> */}
+    </>
+    
+    
+   
+  );
+}
+
+export default App;
---
+++ src/majorproject/API/userInterface.js
@@ -0,0 +1,62 @@
+
+function GetApi() {
+    const url = [
+        {
+            "id": 7,
+            "email": "michael.lawson@reqres.in",
+            "first_name": "Michael",
+            "last_name": "Lawson",
+            "avatar": "https://reqres.in/img/faces/7-image.jpg"
+        },
+        {
+            "id": 8,
+            "email": "lindsay.ferguson@reqres.in",
+            "first_name": "Lindsay",
+            "last_name": "Ferguson",
+            "avatar": "https://reqres.in/img/faces/8-image.jpg"
+        },
+        {
+            "id": 9,
+            "email": "tobias.funke@reqres.in",
+            "first_name": "Tobias",
+            "last_name": "Funke",
+            "avatar": "https://reqres.in/img/faces/9-image.jpg"
+        },
+        {
+            "id": 10,
+            "email": "byron.fields@reqres.in",
+            "first_name": "Byron",
+            "last_name": "Fields",
+            "avatar": "https://reqres.in/img/faces/10-image.jpg"
+        },
+        {
+            "id": 11,
+            "email": "george.edwards@reqres.in",
+            "first_name": "George",
+            "last_name": "Edwards",
+            "avatar": "https://reqres.in/img/faces/11-image.jpg"
+        },
+        {
+            "id": 12,
+            "email": "rachel.howell@reqres.in",
+            "first_name": "Rachel",
+            "last_name": "Howell",
+            "avatar": "https://reqres.in/img/faces/12-image.jpg"
+        }
+    ];
+
+    const filteredUsers = url.filter(user => user.first_name.startsWith('S'));
+
+    return (
+        <>
+            {filteredUsers.map(user => (
+                <div key={user.id}>
+                    <p>Email: {user.email}</p>
+                    <p>Name: {`${user.first_name} ${user.last_name}`}</p>
+                    <img src={user.avatar} alt={`Avatar of ${user.first_name}`} />
+                </div>
+            ))}
+        </>
+    );
+}
+export default GetApi
