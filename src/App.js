d4df66527acde06bb9549bbf9fae7e0b0b9649c5
srilekhasrilu24 <srilekhaambati71@gmail.com>
2024-03-15 10:19:34 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-
-
-import React from 'react';
-import './App.css';
-import MajorProject from './majorproject/majorproject';
+import React from "react";
+import "./App.css";
+import MajorProject from "./majorproject/majorproject";
 // import GetApi from './majorproject/API/userInterface';
-
-
 
 function App() {
   return (
     <>
-    <MajorProject />
-    {/* <GetApi/> */}
+      <MajorProject />
+      {/* <GetApi/> */}
     </>
-    
-    
-   
   );
 }
 
